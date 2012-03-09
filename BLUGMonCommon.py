104125d27f4129c42c007ec53d9f4b2b92a499cc
Simon Carr <simonjcarr@gmail.com>
2012-03-09 23:27:03 +0000

    Added BLUGMonCommon Module with following classes
    -Module
    -ModuleManager
    -Task
    -TaskManager
    -Protocol

---
+++ BLUGMonCommon.py
@@ -0,0 +1,24 @@
+#This module contains all the classes that are common to
+#BLUGMon-Remote
+#BLUGMon-Hub
+
+
+class Module(object):
+    def __init__(self):
+        pass
+    
+class ModuleManager(object):
+    def __init__(self):
+        pass
+    
+class Task(object):
+    def __init__(self):
+        pass
+    
+class TaskManager(object):
+    def __init__(self):
+        pass
+        
+class Protocol(object):
+    def __init__(self):
+        pass
